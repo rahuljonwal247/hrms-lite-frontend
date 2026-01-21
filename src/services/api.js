d566069c28bd03663c9f41b3c18e1021212f4a49
rahuljonwal247 <77151653+rahuljonwal247@users.noreply.github.com>
2026-01-21 21:11:55 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL || 'https://hrms-lite-backend-ix30.onrender.com';
+const API_URL = process.env.REACT_APP_API_URL || 'https://hrms-lite-backend-ix30.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_URL,
